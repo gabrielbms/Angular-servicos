882649a7471d19273e95a71c8fd4cf86179ad441
Gabriel Bueno <gabriel.bueno.m.s@hotmail.com.br>
2022-02-27 16:33:36 -0300

    Comunicação entre componentes usando serviços e Injetando um serviço em outro serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
-import { CursosComponent } from './cursos/cursos.component';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [CursosService],
+  providers: [LogService],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -5,7 +5,8 @@ import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.scss']
+  styleUrls: ['./cursos.component.scss'],
+  providers: [CursosService]
 })
 export class CursosComponent implements OnInit {
 
@@ -14,10 +15,15 @@ export class CursosComponent implements OnInit {
 
   constructor(private cursosService: CursosService) {
     //this.cursosService = new CursosService();
+    //this.cursosService = _cursosService;
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+
+    CursosService.criouNovoCurso.subscribe(
+      curso => this.cursos.push(curso)
+    );
   }
 
 }
